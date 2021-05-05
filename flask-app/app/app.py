e44981a31fe1ce9f915590cd8b003a611325b324
LobePrism <69841431+LobePrism@users.noreply.github.com>
2021-05-06 04:16:51 +0500

    Create app.py

---
+++ flask-app/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+      return 'Hello, world!'
+
+
+@app.route('/')
+def hello_world():
+      return 'Hello, world!'
+
+
+if __name__ == "__main__":
+      app.run()
+ 
