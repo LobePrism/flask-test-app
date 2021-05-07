082be4f64e44c416e7c934455003d3680cf79327
LobePrism <69841431+LobePrism@users.noreply.github.com>
2021-05-07 08:19:05 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socket = SocketIO(app)
 
 
 @app.route('/')
 def hello_world():
-      return 'Hello, world!'
+    return 'Hello, world!'
+
+
+@socket.event("message", namespace="/ws")
+def on_message(message):
+    print(message)
+    emit('my response', {'data': message})
 
 
 if __name__ == "__main__":
-      app.run()
- 
+      socket.run(app)
